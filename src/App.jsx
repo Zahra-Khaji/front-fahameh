a5470b99efd83a5dea589562c36af05104da86c8
Zahra-Khaji <z.khaji.95@gmail.com>
2025-10-28 13:28:21 +0330

    after login redirect to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
               </ProtectedRoute>
             }
           >
-            {/* <Route index element={<Navigate to="dashboard" replace />} /> */}
+            <Route index element={<Navigate to="dashboard" replace />} />
             <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="registerInspection" element={<RegisterInspection />} />
 
