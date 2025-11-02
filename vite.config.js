93014cc8595f9ae7992c84ce2350635ff1ab8ce3
Zahra-Khaji <z.khaji.95@gmail.com>
2025-11-02 16:30:57 +0330

    cleanUp

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+export default defineConfig({
+  plugins: [react()],
+})
 
 
 // vite.config.js
-import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // export default defineConfig({
@@ -19,18 +21,21 @@ import { defineConfig } from 'vite'
 // })
 
 // import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
-  }
-})
+
+
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     chunkSizeWarningLimit: 1000,
+//     rollupOptions: {
+//       output: {
+//         assetFileNames: 'assets/[name]-[hash][extname]',
+//         chunkFileNames: 'assets/[name]-[hash].js',
+//         entryFileNames: 'assets/[name]-[hash].js'
+//       }
+//     }
+//   }
+// })
