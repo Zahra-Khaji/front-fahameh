6704765598186a114f6cc401a4d7e3df50fd74a3
Zahra-Khaji <z.khaji.95@gmail.com>
2025-11-02 16:35:02 +0330

    changed chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-
-// vite.config.js
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     chunkSizeWarningLimit: 1000, // همین خط رو اضافه کنید
-//   },
-// })
-
-// import { defineConfig } from 'vite'
-
-
-
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     chunkSizeWarningLimit: 1000,
-//     rollupOptions: {
-//       output: {
-//         assetFileNames: 'assets/[name]-[hash][extname]',
-//         chunkFileNames: 'assets/[name]-[hash].js',
-//         entryFileNames: 'assets/[name]-[hash].js'
-//       }
-//     }
-//   }
-// })
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+});
